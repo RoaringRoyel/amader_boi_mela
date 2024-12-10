dbfe175cb5aa80e52406b47fa9a935539f37cc25
RoaringRoyel <minhazulroyel001@gmail.com>
2024-12-10 06:03:28 +0600

    Spring Boot Setup

---
+++ src/main/java/com/example/boi/mela/BoiMelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boi.mela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoiMelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoiMelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boi/mela/BoiMelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boi.mela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoiMelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
